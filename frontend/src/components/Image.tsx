1b2c5d7d39ef4f8227965d1b997f9c3b4f61bd09
webxeon <yaxeon@gmail.com>
2019-05-26 20:57:13 +0300

    add cdn

--- frontend/src/components/Image.tsx
+++ frontend/src/components/Image.tsx
@@ -6,7 +6,9 @@ interface Props {
   alt: string;
 }
 
-export const makeUrl = (src: string) => `/object/${src}`;
+const { REACT_APP_CDN = "" } = process.env;
+
+export const makeUrl = (src: string) => `${REACT_APP_CDN}/object/${src}`;
 
 export const Image: React.FC<Props> = ({ src, alt, ...rest }) => (
   <img alt={alt} src={makeUrl(src)} {...rest} />
--- frontend/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-const proxy = require("http-proxy-middleware");
-
-module.exports = function(app) {
-  app.use(proxy(["/object", "/graphql"], { target: "http://localhost:8080/" }));
-};
