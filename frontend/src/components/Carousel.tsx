666f39750174968fc32c6076aae1e578d7383fb1
webxeon <yaxeon@gmail.com>
2019-06-24 21:45:58 +0300

    add zoom

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Theme, makeStyles } from "@material-ui/core/styles";
 import SwipeableViews from "react-swipeable-views";
+// @ts-ignore
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
 
 import { Image } from "./Image";
 
@@ -28,12 +30,14 @@ const useStyles = makeStyles<Theme>(theme => ({
   dotActive: {
     border: "4px solid #000"
   },
+  container: {
+    touchAction: "auto"
+  },
   slide: {
-    maxHeight: theme.spacing(60),
-    textAlign: "center",
     "& > img": {
-      maxHeight: theme.spacing(60),
-      maxWidth: "100%"
+      maxWidth: "100%",
+      display: "block",
+      height: "auto"
     }
   }
 }));
@@ -45,17 +49,31 @@ interface Props {
 export const Carousel: React.FC<Props> = ({ images }) => {
   const classes = useStyles();
   const [current, setCurrent] = useState(0);
+  const [transform, setTransform] = useState("");
 
   const handleChangeIndex = (index: number) => {
     setCurrent(index);
+    setTransform(make3dTransformValue({ x: 0, y: 0, scale: 1 }));
+  };
+
+  const onUpdate = ({ x, y, scale }: any) => {
+    setTransform(make3dTransformValue({ x, y, scale }));
   };
 
   return (
     <div className={classes.root}>
       <SwipeableViews enableMouseEvents onChangeIndex={handleChangeIndex}>
         {images.map(src => (
-          <div key={src} className={classes.slide}>
-            <Image alt="Image" src={src} />
+          <div key={src}>
+            <QuickPinchZoom
+              onUpdate={onUpdate}
+              draggableUnZoomed={false}
+              containerProps={{ className: classes.container }}
+            >
+              <div className={classes.slide} style={{ transform }}>
+                <Image alt="Image" src={src} />
+              </div>
+            </QuickPinchZoom>
           </div>
         ))}
       </SwipeableViews>
