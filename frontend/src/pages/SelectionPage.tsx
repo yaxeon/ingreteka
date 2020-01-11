ee5cb13f2b9810bad96a3ca5272868a9101ca0f5
webxeon <yaxeon@gmail.com>
2020-01-11 17:09:27 +0200

    add donate png

--- frontend/src/pages/SelectionPage.tsx
+++ frontend/src/pages/SelectionPage.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import { RouteComponentProps } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
+import debounce from "lodash/debounce";
 
 import idx from "idx";
 
@@ -16,11 +17,30 @@ import { Carousel } from "../components/Carousel";
 import { Container } from "../components/Layout";
 import { Favorite } from "../components/Favorite";
 import { InstagramIcon } from "../components/InstagramIcon";
+import donate from "../svg/donate.png";
 
 const useStyles = makeStyles({
   action: {
     display: "flex",
     justifyContent: "flex-end"
+  },
+  donate: {
+    position: "fixed",
+    transition: "all 0.3s",
+    bottom: "64px",
+    right: "32px",
+    "z-index": "2000",
+    "& img": {
+      width: "120px"
+    }
+  },
+  donateShow: {
+    opacity: 1,
+    transform: "rotate(0deg) scale(1)"
+  },
+  donateHide: {
+    opacity: 0,
+    transform: "rotate(360deg) scale(0.5)"
   }
 });
 
@@ -34,6 +54,25 @@ export const SelectionPage: React.FC<RouteComponentProps<PageParams>> = ({
   const classes = useStyles();
   const { data, loading } = useSelectionItemQuery({ variables: { id } });
   const selection = idx(data, _ => _.selection.item);
+  const [isDonate, setDonate] = useState(false);
+
+  const handleScroll = () => {
+    const offset = document.documentElement.scrollTop + window.innerHeight;
+    const height = document.documentElement.offsetHeight;
+    console.log("set");
+    setDonate(offset + 30 > height);
+  };
+
+  useEffect(() => {
+    const handle = debounce(handleScroll, 60);
+    window.addEventListener("scroll", handle);
+
+    return () => {
+      window.removeEventListener("scroll", handle);
+      console.log("cancel");
+      handle.cancel();
+    };
+  }, []);
 
   if (loading || !selection) {
     return <Loading />;
@@ -58,6 +97,10 @@ export const SelectionPage: React.FC<RouteComponentProps<PageParams>> = ({
       })
     : null;
 
+  const donateClass = `${classes.donate} ${
+    isDonate ? classes.donateShow : classes.donateHide
+  }`;
+
   return (
     <React.Fragment>
       <Helmet>
@@ -66,6 +109,11 @@ export const SelectionPage: React.FC<RouteComponentProps<PageParams>> = ({
       <HeaderMenu backUri={`/category/${slug}/`}>
         {categoryBySlug ? categoryBySlug.title : slug}
       </HeaderMenu>
+      <div className={donateClass}>
+        <a href="https://money.yandex.ru/to/410015427968355?_openstat=iget%3Bbill%3Bicon">
+          <img alt="Поддержать!" src={donate} />
+        </a>
+      </div>
       <Container>
         <Carousel images={gallery} />
         <div className={classes.action}>
